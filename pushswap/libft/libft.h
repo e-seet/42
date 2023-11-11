c32461a7e4f8d94d19a822fd168d2173c2be4fb6
2202210 <2202210@sit.singaporetech.edu.sg>
2023-11-12 01:38:02 +0800

    initial commit for pushswap

---
+++ pushswap/libft/libft.h
@@ -0,0 +1,10 @@
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_atoi(const char *str);
+
+#endif
---
+++ pushswap/utils.h
@@ -0,0 +1,15 @@
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include <sys/wait.h>
+// # include "./libft/libft.h"
+# include <stdio.h>
+# include <unistd.h>
+//for read write, permission with files, creating files
+# include <fcntl.h>
+
+int	ft_atoi(const char *str);
+
+
+#endif
