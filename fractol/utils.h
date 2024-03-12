a8e67de4eb278d6dda602697b30ddd3848ca22eb
2202210 <2202210@sit.singaporetech.edu.sg>
2024-03-12 12:53:02 +0800

    init

---
+++ fractol/utils.h
@@ -0,0 +1,12 @@
+
+#ifndef UTILS_H
+# define UTILS_H
+
+# include "../libft/libft.h"
+# include <unistd.h>
+//for read write, permission with files, creating files
+# include <fcntl.h>
+# include <stdlib.h>
+
+
+#endif
